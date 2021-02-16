23ebd18f4e9d65a56e2889cc30b7d871ad588c78
Vasyl Hevko <hevko.vasyl.me@gmail.com>
2021-02-16 02:35:10 +0200

    123

--- src/index.js
+++ src/index.js
@@ -1,1 +1,40 @@
 import './styles.css';
+import debounce from 'lodash.debounce';
+import refs from './js/refs';
+import fetchCountry from './js/fetchCountries';
+import countryListMarkup from './js/countryListMarkup';
+import countryMarkup from './js/countryMarkup';
+import error from './js/notifications';
+
+
+refs.input.addEventListener('input', debounce(search), 500);
+
+function search(event) {
+    const countryName = event.target.value;
+    refs.countryList.innerHTML = '';
+    refs.countryContainer.innerHTML = '';
+    fetchCountry(countryName).then(country => {
+        if (country.length === 1) {
+            countryMarkup(country)
+            return
+        };
+        if (country.length < 10) {
+            countryListMarkup(country)
+            return
+        };
+        if (country.length > 10) {
+            countryListMarkup(country)
+            error({
+                text: 'Too many matches found. Please enter a more specific query!',
+                delay: 1000
+            });
+            return
+        };
+    });
+};
+
+
+
+
+
+
